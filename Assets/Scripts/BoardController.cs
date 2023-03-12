dc1f939a02446c6994025f7d33a9463c107582ed
Aditya Jain <jainaditya400@gmail.com>
2023-03-12 17:50:56 +0530

    uix

--- Assets/Scripts/BoardController.cs
+++ Assets/Scripts/BoardController.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class BoardController : MonoBehaviour
 {
@@ -9,12 +11,31 @@ public class BoardController : MonoBehaviour
     private GameObject tile;
     [SerializeField]
     private Transform boardParent;
+    private TileController[,] currentTileSet;
+    [SerializeField]
     private GridLayoutGroup grid;
     private LevelData levelData;
+    [SerializeField]
+    private TextMeshProUGUI levelInfo;
+    public static BoardController Instance { get; private set; }
+
+    private void Awake()
+    {
+
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
     private void Start()
     {
+        
         levelData = MultiSceneValues.Instance.levelValue;
-        grid = this.gameObject.GetComponent<GridLayoutGroup>();
+        currentTileSet = new TileController[levelData.gridSize, levelData.gridSize];
         SetBoard();
 
     }
@@ -30,28 +51,86 @@ public class BoardController : MonoBehaviour
         {
             grid.cellSize = new Vector2(110, 110);
         }
+
         for(int i = 0; i < levelData.gridSize; i++)
         {
             for (int j = 0; j < levelData.gridSize; j++)
-            {
+            { 
                 GameObject obj = Instantiate(tile, boardParent);
                 TileController tileController = obj.GetComponent<TileController>();
                 if (levelData.problem[i, j] == -1)
                 {
                     tileController.textMesh.text = "";
+                    tileController.tileValue = -1;
                 }
                 else if (levelData.problem[i, j] == 1)
                 {
                     tileController.textMesh.text = "1";
+                    tileController.tileValue = 1;
                     tileController.tileButton.interactable = false;
                 }
                 else if (levelData.problem[i, j] == 0)
                 {
                     tileController.textMesh.text = "0";
+                    tileController.tileValue = 0;
                     tileController.tileButton.interactable = false;
+                }
+                tileController.tileValue = levelData.problem[i, j];
+                currentTileSet[i, j] = tileController;
+
+            }
+        }
+        levelInfo.text = "#" + levelData.id + " | " + levelData.gridSize + "x" + levelData.gridSize + " | dif= " + levelData.difficulty;
+    }
+
+    public void CheckForWin()
+    {
+        bool flag = true;
+        for (int i = 0; i < levelData.gridSize; i++)
+        {
+            for (int j = 0; j < levelData.gridSize; j++)
+            {
+                if (currentTileSet[i, j].tileValue != levelData.solution[i, j])
+                {
+                    flag = false;
+                    break;
+                }       
+            }
+            if (!flag)
+            {
+                break;
+            }
+        }
+        if (flag)
+        {
+            Debug.Log("win");
+        } 
+    }
+
+    public void HintSystem()
+    {
+        for (int i = 0; i < levelData.gridSize; i++)
+        {
+            for (int j = 0; j < levelData.gridSize; j++) 
+            {
+                if (currentTileSet[i, j].tileValue == -1)
+                {
+                    currentTileSet[i, j].tileValue = levelData.solution[i, j];
+                    currentTileSet[i, j].textMesh.text = levelData.solution[i, j].ToString();
+                    CheckForWin();
+                    return;
                 }
             }
         }
+    }
 
+    public void ResetBoard()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene(0);
     }
 }
--- Assets/Scripts/TileController.cs
+++ Assets/Scripts/TileController.cs
@@ -8,20 +8,26 @@ public class TileController : MonoBehaviour
 {
     public TextMeshProUGUI textMesh;
     public Button tileButton;
+    public int tileValue;
 
     public void OnTileClick()
     {
         if (textMesh.text == "")
         {
             textMesh.text = "0";
+            tileValue = 0;
+            BoardController.Instance.CheckForWin();
         }
         else if (textMesh.text=="0")
         {
             textMesh.text = "1";
+            tileValue = 1;
+            BoardController.Instance.CheckForWin();
         }
         else
         {
             textMesh.text = "";
+            tileValue = -1;
         }
     }
 }
